22a2770045e2751393d0cfdd0afb066e1a5cd04c
Anshuman <anshuman.patil@figmd.in>
2019-07-03 20:21:56 +0530

    added routing

---
+++ src/components/forgot/Forgot.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Forgot() {
+  return (
+    <div>
+      Forgot
+    </div>
+  );
+}
+
+export default Forgot;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React , {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import  Register  from '../components/register/Register'
+import  Login  from '../components/login/Login'
+import  Forgot  from '../components/forgot/Forgot'
+
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
+    <Router>
       Initial
-    </div>
-  );
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/register/">Register</Link>
+        <Link to="/forgot/">Forgot</Link>
+      </div>
+      
+        <Route path="/" exact component={Login} />
+        <Route path="/register/" component={Register} />
+        <Route path="/forgot/" component={Forgot} />
+    </Router>
+  </div> );
+
+  }
 }
 
 export default App;
