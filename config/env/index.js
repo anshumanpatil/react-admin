dc3b2fa5402aee76ebbd9d71f0d6d45ee84122de
anshumanpatil <anshumanpradippatil1506@gmail.com>
2020-02-22 20:49:49 +0530

    docker file added

--- config/env/index.js
+++ config/env/index.js
@@ -1,7 +1,7 @@
 var nconf = require('nconf');
 nconf.argv().env();
 var path = require('path');
-var environment = nconf.get('NODE_ENV') || 'office';
+var environment = nconf.get('NODE_ENV') || 'home';
 nconf.file(path.resolve(__dirname, 'default.json'));
 nconf.file(path.resolve(__dirname, environment + '_config.json'));
 
